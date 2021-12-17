59e9e10f7a2711ca97dbbbacb6da224f9e42570c
kento.okuda@asterone.co.jp <kento.okuda@asterone.co.jp>
2021-12-17 06:08:29 +0000

    バックエンドとの繋ぎこみ前 複数ユーザーと自分のアイコン表示

--- components/RideMap.js
+++ components/RideMap.js
@@ -1,71 +1,209 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { Dimensions, StyleSheet, Text, View, ActivityIndicator, PermissionsAndroid } from 'react-native'
-import MapView, { Marker } from 'react-native-maps';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Dimensions, StyleSheet, Text, View, Image, ActivityIndicator, PermissionsAndroid } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 // import { useDispatch, useSelector } from 'react-redux';
-import tw from 'tailwind-react-native-classnames';
+import tw from "tailwind-react-native-classnames";
 // import { selectDestination, selectOrigin, setTravelInformation } from '../slices/navSlice';
 // import MapViewDirections from 'react-native-maps-directions';
 // import { Tooltip } from 'react-native-elements';
 // import { GOOGLE_MAPS_API_KEY } from '@env'
 // import { globalUserContext } from '../global/globalContext';
 // import { requestPermission } from '../utils/mapUtils'
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
+import { button } from "aws-amplify";
 // const { width, height } = Dimensions.get('window');
+const avatar_1 = require("../assets/images/avatar-1.jpg");
+const avatar_2 = require("../assets/images/avatar-2.jpg");
 
-const initial = {
-    latitude: 37.78825,
-    longitude: -122.4324,
+//ダミーデータ
+const people1 = {
+    latitude: undefined,
+    longitude: undefined,
     latitudeDelta: 0.05,
     longitudeDelta: 0.05,
-}
-
+    title: "Marker 1",
+    description: "This is marker1",
+    avatar: avatar_1,
+};
+const people2 = {
+    latitude: undefined,
+    longitude: undefined,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+    title: "Marker 2",
+    description: "This is marker2",
+    avatar: avatar_2,
+};
+const initial = {
+    latitude: 37.3469,
+    longitude: -122.0121,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+};
 
 const Map = () => {
-    const [currentLocation, setCurrentLocation] = useState(initial)
+    const [currentLocation, setCurrentLocation] = useState(initial);
+    const [peopleMarkers, setPeopleMarkers] = useState([]);
+    const [region, setRegion] = useState(initial);
+    const [myMarker, setMyMarker] = useState(null);
     const mapRef = useRef(null);
 
     useEffect(() => {
         (async () => {
+            //初めての位置情報取得
             let { status } = await Location.requestForegroundPermissionsAsync();
             if (status !== PermissionsAndroid.RESULTS.GRANTED) {
-                console.log('Permission to access foreground location was denied');
+                console.log("Permission to access foreground location was denied");
                 return;
             }
-
             let location = await Location.getCurrentPositionAsync({
-                enableHighAccuracy: true
+                enableHighAccuracy: true,
             });
-            setCurrentLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
-            console.log("location==> ", location);
+            //マップの中央を現在地に設定
+            setRegion({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
+
+            //定期的(5秒)に他ユーザーの位置情報を取得する（繋ぎこみ前のテスト用）
+            const interval = setInterval(() => {
+                people1.longitude = location.coords.longitude + getRandomNum(-0.001, 0.001);
+                people2.longitude = location.coords.longitude + getRandomNum(-0.001, 0.001);
+                people1.latitude = location.coords.latitude + getRandomNum(-0.001, 0.001);
+                people2.latitude = location.coords.latitude + getRandomNum(-0.001, 0.001);
+                setPeopleMarkers([people1, people2]); //マーカの位置を変更
+            }, 5000);
+            return () => clearInterval(interval);
+            // setCurrentLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
+            // console.log("location==> ", location);
         })();
 
         return () => setCurrentLocation(initial);
-    }, [])
+    }, []);
 
-    
-    
+    const getRandomNum = (min, max) => {
+        return Math.random() * (max - min) + min;
+    };
+
+    let i = 0;
+
+    // const getPositionCallBack = location => {
+    //     console.log("loc==> ", location);
+    //     // setMyMarker({
+    //     //     latitude: location.coords.latitude,
+    //     //     longitude: location.coords.longitude,
+    //     //     latitudeDelta: 0.05,
+    //     //     longitudeDelta: 0.05,
+    //     //     title: "MY Marker ",
+    //     //     description: "This is My marker",
+    //     // });
+    //     // setCurrentLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 })
+    // };
+
+    // //位置情報を継続して監視する
+    // useEffect(() => {
+    //     let subscriber;
+    //     const startWatching = async () => {
+    //         try {
+    //             const { status } = await Location.requestForegroundPermissionsAsync();
+    //             subscriber = await Location.watchPositionAsync(
+    //                 {
+    //                     accuracy: Location.Accuracy.Highest,
+    //                     timeInterval: 5000,
+    //                     distanceInterval: 10,
+    //                 },
+    //                 getPositionCallBack
+    //             );
+    //             if (status !== "granted") {
+    //                 throw new Error("Location permission not granted");
+    //             }
+    //         } catch (err) {
+    //             console.log("Error: ", err);
+    //             setError(err);
+    //         }
+    //     };
+
+    //     startWatching();
+
+    //     /*DBに現在の位置情報を保存する*/
+
+    //     return () => {
+    //         if (subscriber) {
+    //             subscriber.remove();
+    //         }
+    //         setCurrentLocation(initial);
+    //     };
+    // }, [getPositionCallBack]);
 
     return (
         <MapView
             style={tw`h-full`}
             ref={mapRef}
-            initialRegion={currentLocation}
-            region={currentLocation}
-            showsUserLocation={true}
-            showsMyLocationButton={false}
-            onUserLocationChange={(e) => {
-                console.log("user location change", e);
-                setCurrentLocation({ ...e.nativeEvent.coordinate, latitudeDelta: 0.05, longitudeDelta: 0.05 });
+            initialRegion={initial}
+            region={region}
+            onRegionChange={loc => {
+                setRegion(loc);
             }}
-        />
-        // <Text>Map</Text>
-    )
-}
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            onUserLocationChange={e => {
+                // console.log("user location change", e.nativeEvent);
+                setCurrentLocation({ ...e.nativeEvent.coordinate, latitudeDelta: 0.05, longitudeDelta: 0.05 });
 
-export default Map
+                /*DBに現在の位置情報を保存する*/
+            }}
+        >
+            {peopleMarkers.map((marker, index) => (
+                <Marker
+                    key={index}
+                    coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                    title={marker.title}
+                    description={marker.description}
+                >
+                    <View
+                        style={{
+                            height: 40,
+                            width: 40,
+                            borderRadius: 20,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            backgroundColor: "#54e346",
+                        }}
+                    >
+                        <View
+                            style={{
+                                height: 32,
+                                width: 32,
+                                borderRadius: 15,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                backgroundColor: "white",
+                            }}
+                        >
+                            <Image
+                                style={{
+                                    width: 27,
+                                    height: 27,
+                                    // tintColor: COLORS.white
+                                }}
+                            />
+                        </View>
+                    </View>
+                </Marker>
+            ))}
+            {/* {myMarker && (
+                <Marker
+                    coordinate={{ latitude: myMarker.latitude, longitude: myMarker.longitude }}
+                    title={myMarker.title}
+                    description={myMarker.description}
+                />
+            )} */}
+        </MapView>
+
+        // <Text>Map</Text>
+    );
+};
+
+export default Map;
 
 // const styles = StyleSheet.create({})
-
 
 // const Loading = () => {
 //     return (
