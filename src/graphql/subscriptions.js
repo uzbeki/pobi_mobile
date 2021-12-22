375209403226bbe1500b64cc28277eb4c60f6292
t.nagakura <tensei.nagakura@asterone.co.jp>
2021-12-22 13:38:17 +0900

    Add subscription to 'onUpdatePeopleLocation' by ride_event

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,6 +1,19 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const onUpdatePeopleLocationByRideEvent = /* GraphQL */ `
+  subscription OnUpdatePeopleLocationByRideEvent($ride_event: ID!) {
+    onUpdatePeopleLocationByRideEvent(ride_event: $ride_event) {
+      ride_event
+      user
+      longitude
+      latitude
+      speed
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const onCreatePeopleLocation = /* GraphQL */ `
   subscription OnCreatePeopleLocation {
     onCreatePeopleLocation {
