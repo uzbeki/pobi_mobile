bcd012c98a80c1229012668ca0ceecbeb9ed1850
kento.okuda@asterone.co.jp <kento.okuda@asterone.co.jp>
2021-12-23 09:49:42 +0000

    regionの変更のバグ修正途中

--- components/RideMap.js
+++ components/RideMap.js
@@ -19,44 +19,23 @@ import { tryUpdatePeopleLocation } from "../utils/PeopleLocation";
 const avatar_1 = require("../assets/images/avatar-1.jpg");
 const avatar_2 = require("../assets/images/avatar-2.jpg");
 
-//ダミーデータ
-// const people1 = {
-//     latitude: undefined,
-//     longitude: undefined,
-//     latitudeDelta: 0.05,
-//     longitudeDelta: 0.05,
-//     title: "Marker 1",
-//     description: "This is marker1",
-//     avatar: avatar_1,
-// };
-// const people2 = {
-//     latitude: undefined,
-//     longitude: undefined,
-//     latitudeDelta: 0.05,
-//     longitudeDelta: 0.05,
-//     title: "Marker 2",
-//     description: "This is marker2",
-//     avatar: avatar_2,
-// };
 const initial = {
-    latitude: 37.3469,
-    longitude: -122.0121,
+    latitude: 35.6804,
+    longitude: 139.769,
     latitudeDelta: 0.05,
     longitudeDelta: 0.05,
 };
 const myUserInfo = { id: "okuda" };
+// const myUserInfo = { id: "beki" };
 
 const Map = () => {
-    const [currentLocation, setCurrentLocation] = useState(initial);
+    const [myLocation, setMyLocation] = useState(initial);
     const [peopleMarkers, setPeopleMarkers] = useState([]);
-    const [region, setRegion] = useState(initial);
-    const [myMarker, setMyMarker] = useState(null);
-
-    const [shouldTrack, setShouldTrack] = useState(true);
+    const [regionByUser, setRegionByUser] = useState(null);
+    // const [myMarker, setMyMarker] = useState(null);
+    // const [shouldTrack, setShouldTrack] = useState(true);
     const mapRef = useRef(null);
-    const getRandomNum = (min, max) => {
-        return Math.random() * (max - min) + min;
-    };
+
     useEffect(() => {
         let subscriptionToUpdateLocation;
         (async () => {
@@ -70,17 +49,8 @@ const Map = () => {
                 enableHighAccuracy: true,
             });
             //マップの中央を現在地に設定
-            setRegion({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
-
-            //定期的(5秒)に他ユーザーの位置情報を取得する（繋ぎこみ前のテスト用）
-            // const interval = setInterval(() => {
-            //     people1.longitude = location.coords.longitude + getRandomNum(-0.001, 0.001);
-            //     people2.longitude = location.coords.longitude + getRandomNum(-0.001, 0.001);
-            //     people1.latitude = location.coords.latitude + getRandomNum(-0.001, 0.001);
-            //     people2.latitude = location.coords.latitude + getRandomNum(-0.001, 0.001);
-            //     setPeopleMarkers([people1, people2]); //マーカの位置を変更
-            // }, 5000);
-
+            // setRegion({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
+            setMyLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
             const subscribeToUpdateLocation = async () => {
                 subscriptionToUpdateLocation = await API.graphql({
                     query: onUpdatePeopleLocationByRideEvent,
@@ -91,27 +61,34 @@ const Map = () => {
                     next: event => {
                         console.log("subscription");
                         const newUserLocation = event.value.data.onUpdatePeopleLocationByRideEvent;
-                        console.log("newUserLocation =>", newUserLocation);
-                        //自分の情報を除外
-                        if (newUserLocation.user === myUserInfo.id) return;
-                        //新しい位置情報と一致するマーカーのインデックスを取得
-                        const newUserLocIndex = peopleMarkers.findIndex(marker => marker.user === newUserLocation.user);
-                        if (newUserLocIndex === -1) {
-                            //新規ユーザーのマーカーを追加
-                            console.log("add new user");
-                            peopleMarkers.push(newUserLocation);
-                            setPeopleMarkers(peopleMarkers);
+                        // console.log("newUserLocation =>", newUserLocation);
+
+                        if (newUserLocation.user === myUserInfo.id) {
+                            console.log("ほかのユーザーの位置情報が取得できません。");
+
+                            return;
                         } else {
-                            //既存ユーザーのマーカーを更新
-                            console.log("update marker");
-                            peopleMarkers[newUserLocIndex] = {
-                                ...newUserLocation,
-                                title: newUserLocation.user,
-                                description: `This is ${newUserLocation.user}`,
-                                avatar: avatar_1,
-                                // avatar: avatar_2,
-                            };
-                            setPeopleMarkers(peopleMarkers);
+                            //新しい位置情報と一致するマーカーのインデックスを取得
+                            const newUserLocIndex = peopleMarkers.findIndex(
+                                marker => marker.user === newUserLocation.user
+                            );
+                            if (newUserLocIndex === -1) {
+                                //新規ユーザーのマーカーを追加
+                                console.log("add new user");
+                                peopleMarkers.push(newUserLocation);
+                                setPeopleMarkers(peopleMarkers);
+                            } else {
+                                //既存ユーザーのマーカーを更新
+                                console.log("update marker");
+                                peopleMarkers[newUserLocIndex] = {
+                                    ...newUserLocation,
+                                    title: newUserLocation.user,
+                                    description: `This is ${newUserLocation.user}`,
+                                    avatar: avatar_1,
+                                    // avatar: avatar_2,
+                                };
+                                setPeopleMarkers(peopleMarkers);
+                            }
                         }
                     },
                     error: error => console.warn(error),
@@ -119,13 +96,11 @@ const Map = () => {
             };
             subscribeToUpdateLocation();
 
-            return () => clearInterval(interval);
-            // setCurrentLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
+            // setMyLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 });
             // console.log("location==> ", location);
         })();
 
         return () => {
-            setCurrentLocation(initial);
             subscriptionToUpdateLocation.unsubscribe();
         };
     }, []);
@@ -151,7 +126,7 @@ const Map = () => {
     //         }
     //     }
 
-    //     // setCurrentLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 })
+    //     // setMyLocation({ ...location.coords, latitudeDelta: 0.05, longitudeDelta: 0.05 })
     // };
 
     // //位置情報を継続して監視する
@@ -191,7 +166,7 @@ const Map = () => {
     //         if (subscriber) {
     //             subscriber.remove();
     //         }
-    //         setCurrentLocation(initial);
+    //         setMyLocation(initial);
     //     };
     // }, [shouldTrack,getPositionCallBack]);
 
@@ -200,22 +175,34 @@ const Map = () => {
             style={tw`h-full`}
             ref={mapRef}
             initialRegion={initial}
-            // region={region}
-            // onRegionChange={loc => {
-            //     setRegion(loc);
+            region={
+                !regionByUser ? myLocation : regionByUser
+                // myLocation.latitude === region.latitude && myLocation.longitude === region.longitude
+                //     ? myLocation
+                //     : false
+            }
+            // onRegionChangeComplete={(region, { isGesture }) => {
+            //     // console.log("region change complete", isGesture);
+            //     // if (isGesture) {
+            //     //     console.log("isGesture");
+            //     //     setRegionByUser(region);
+            //     // } else {
+            //     //     console.log("not gesture (showsMyLocationButton)");
+            //     //     setRegionByUser(null);
+            //     // }
             // }}
+            followsUserLocation={true} //for iOS only
             showsUserLocation={true}
             showsMyLocationButton={true}
             onUserLocationChange={e => {
                 const coordinate = e.nativeEvent.coordinate;
                 // console.log("user location change", e.nativeEvent);
-                setCurrentLocation({ ...e.nativeEvent.coordinate, latitudeDelta: 0.05, longitudeDelta: 0.05 });
+                setMyLocation({ ...e.nativeEvent.coordinate, latitudeDelta: 0.05, longitudeDelta: 0.05 });
 
                 /*DBに現在の位置情報を保存する*/
                 tryUpdatePeopleLocation({
                     ride_event: "test0",
-                    user: "okuda",
-                    // user: "beki",
+                    user: myUserInfo.id,
                     latitude: coordinate.latitude,
                     longitude: coordinate.longitude,
                 });
